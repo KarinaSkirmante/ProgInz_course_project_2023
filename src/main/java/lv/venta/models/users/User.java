b5f557240018b15e1b592e85c4a1e82d782b5798
localuser <localuser@VEA.LV>
2023-05-19 11:46:00 +0300

    Add: Course model class

--- src/main/java/lv/venta/models/users/User.java
+++ src/main/java/lv/venta/models/users/User.java
@@ -46,7 +46,6 @@ public class User {
 	private String email;
 
 	public User(@NotNull String password, @NotNull @Email String email) {
-		super();
 		this.password = password;
 		this.email = email;
 	}
